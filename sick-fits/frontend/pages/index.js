e3895a7064e1037f935094f3bb1617fc8dcfaed5
Zak Laughton <zlaughton@atlassian.com>
2019-10-02 17:18:27 -0500

    finish 6 - themes and layout with styled components

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hey!</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Sell!</p>
+    </div>
+  );
+};
+
+export default Home;
